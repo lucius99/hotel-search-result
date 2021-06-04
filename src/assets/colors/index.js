26a1f30a65a66e96e48223cfdbd76beff81345e8
lucius99 <47803507+lucius99@users.noreply.github.com>
2021-06-04 23:39:33 +0700

    Create index.js

---
+++ src/assets/colors/index.js
@@ -0,0 +1,13 @@
+const MAIN_COLOR = "#9E459F";
+const SECONDARY_COLOR = "#BD99F6";
+const BLACK_COLOR = "#000000";
+const WHITE_COLOR = "#FFFFFF";
+
+const colors = {
+  MAIN_COLOR,
+  SECONDARY_COLOR,
+  BLACK_COLOR,
+  WHITE_COLOR,
+};
+
+export default colors;
